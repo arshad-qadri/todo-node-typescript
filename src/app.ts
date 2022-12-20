1dde9c4460e14a03b7e7f3a7d0fb14b9a50fc038
arshad-qadri <arshad.q@caprium.com>
2022-12-20 17:34:28 +0530

    just chages on

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import connects from './config/db';
 import router from './routes/routes';
 import bodyParser from "body-parser"
+import cors from "cors"
 const app = express();
 const port = 4000;
 
 // connecting to database
 connects()
-
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api",router)
